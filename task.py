c125a7a719a4802c68944254b7eaf0896cb34341
usman <usmanhaiderpk02@gmail.com>
2022-02-22 10:48:17 +0500

    this is first project

---
+++ task.py
@@ -0,0 +1,21 @@
+from RPA.Excel.Files import Files
+
+lib = Files()
+
+
+def read_excel_worksheet(path, worksheet):
+    lib.open_workbook(path)
+    return lib.read_worksheet_as_table(worksheet)
+
+
+result = read_excel_worksheet("/home/usman/Downloads/EZ1-Mapping-File.xlsx", "Document Type")
+citation = []
+petition = []
+for i in result:
+    for (key, value) in i.items():
+        if (key, value) == ("B", "Citation"):
+            citation.append(i["A"])
+        elif (key, value) == ("B", "Petition"):
+            petition.append(i["A"])
+print(citation)
+print(petition)
